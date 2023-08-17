00b11402a84c83b05db0aea945a270ef04eda81f
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-08-17 11:08:48 -0300

    Add files via upload

---
+++ 13_Regresion_Polinomica_Residual_Plot.py
@@ -0,0 +1,36 @@
+#GRAFICA EL RESIDUAL PLOT PARA UNA REGRESIÓN POLINÓMICA
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Cargar los datos y preparar el Data Frame
+df = pd.read_csv(r'D:\Germán\Desktop\Python Files\automobile.csv')
+df = df.dropna(axis=0)
+
+# Separar la variable independiente predictora (x) y la variable dependiente objetivo (y)
+x = df[['horsepower']]
+y = df[['price']]
+
+# Transformar las características en una matriz polinómica
+poly = PolynomialFeatures(degree=2)
+x_poly = poly.fit_transform(x)
+
+# Ajustar el modelo de regresión lineal a los datos polinómicos
+model = LinearRegression()
+model.fit(x_poly, y)
+
+# Calcular los residuos
+y_pred = model.predict(x_poly)
+residuals = y - y_pred
+
+# Crear el gráfico de residuos
+plt.figure(figsize=(8, 6))
+plt.scatter(y_pred, residuals, color='blue')
+plt.axhline(y=0, color='red', linestyle='--')
+plt.title('Residual Plot de la Regresión Polinómica')
+plt.xlabel('Valores Predichos')
+plt.ylabel('Residuos')
+plt.show()
