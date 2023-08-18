566f34fcbf9a00fc034837aa80d0abb7b540c0e2
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-08-18 11:23:16 -0300

    Add files via upload

---
+++ 16_Regresion_Polinomica_Multiple_Distribution_Plot.py
@@ -0,0 +1,37 @@
+#GRAFICA EL DISTRIBUTION PLOT PARA UNA REGRESIÓN POLINÓMICA MÚLTIPLE
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Cargar los datos y preparar el Data Frame
+df = pd.read_csv(r'D:\Germán\Desktop\Python Files\automobile.csv')
+df = df.dropna(axis=0)
+
+# Separar las variables predictoras (x) y la variable dependiente objetivo (y)
+x = df[['horsepower', 'curb-weight', 'engine-size', 'width']]
+y = df[['price']]
+
+# Transformar las características en una matriz polinómica
+poly = PolynomialFeatures(degree=2)
+x_poly = poly.fit_transform(x)
+
+# Ajustar el modelo de regresión lineal a los datos polinómicos
+model = LinearRegression()
+model.fit(x_poly, y)
+
+# Calcular los valores predichos y los residuos
+y_pred = model.predict(x_poly)
+residuals = y - y_pred
+
+# Crear el gráfico de distribución de residuos en el estilo deseado
+plt.figure(figsize=(8, 6))
+ax1 = sns.distplot(y, hist=False, color='r', label='Datos reales')
+sns.distplot(y_pred, hist=False, color='b', label='Datos predichos', ax=ax1)
+plt.title('Distribution Plot de la Regresión Polinómica Múltiple')
+plt.xlabel('Valores de Precio')
+plt.ylabel('Densidad')
+plt.legend()
+plt.show()
