4f7f0dd4fe5e2eb855441a7ea280b0a946fe6bbc
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-08-21 20:27:16 -0300

    Add files via upload

---
+++ 18_Regresion_Lineal_Multiple_MSE.py
@@ -0,0 +1,21 @@
+#CALCULA EL MSE PARA UNA REGRESIÓN LINEAL MÚLTIPLE
+
+# Cargar los datos y preparar el Data Frame
+import pandas as pd
+df = pd.read_csv(r'D:\Germán\Desktop\Python Files\automobile.csv')
+df = df.dropna(axis=0)
+
+#Crear y entrenar el modelo
+from sklearn.linear_model import LinearRegression
+lr_model = LinearRegression()
+x = df[['horsepower', 'curb-weight', 'engine-size', 'width']]
+y = df[['price']]
+lr_model.fit(x, y)
+
+#Hacer predicciones
+y_pred = lr_model.predict(x)
+
+#Calcular el MSE
+from sklearn.metrics import mean_squared_error
+mse = mean_squared_error(y, y_pred)
+print("Mean Squared Error:", mse)
