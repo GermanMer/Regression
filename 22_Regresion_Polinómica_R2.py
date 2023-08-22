79394cb7f0cca773755222ed3173681ffe48c59e
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-08-22 11:20:58 -0300

    Add files via upload

---
+++ 22_Regresion_Polinómica_R2.py
@@ -0,0 +1,28 @@
+#CALCULA EL R SQUARED PARA UNA REGRESIÓN POLINÓMICA SIMPLE
+
+# Cargar los datos y preparar el Data Frame
+import pandas as pd
+df = pd.read_csv(r'D:\Germán\Desktop\Python Files\automobile.csv')
+df = df.dropna(axis=0)
+
+# Separar la variable independiente predictora (x) y la variable dependiente objetivo (y)
+x = df[['horsepower']]
+y = df[['price']]
+
+# Transformar las características en una matriz polinómica
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+poly = PolynomialFeatures(degree=2)
+x_poly = poly.fit_transform(x)
+
+# Ajustar el modelo de regresión lineal a los datos polinómicos
+model = LinearRegression()
+model.fit(x_poly, y)
+
+# Predecir valores utilizando el modelo ajustado
+y_pred = model.predict(x_poly)
+
+#Calcular el R Squared
+from sklearn.metrics import r2_score
+r2 = r2_score(y, y_pred)
+print("R Squared:", r2)
