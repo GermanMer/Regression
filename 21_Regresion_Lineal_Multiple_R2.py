b288571dc8ce40e3b626a55004a2eea52421b0b8
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-08-22 11:20:42 -0300

    Add files via upload

---
+++ 21_Regresion_Lineal_Multiple_R2.py
@@ -0,0 +1,21 @@
+#CALCULA EL R SQUARED PARA UNA REGRESIÓN LINEAL MÚLTIPLE
+
+# Cargar los datos y preparar el Data Frame
+import pandas as pd
+df = pd.read_csv(r'D:\Germán\Desktop\Python Files\automobile.csv')
+df = df.dropna(axis=0)
+
+#Crear y entrenar el modelo
+from sklearn.linear_model import LinearRegression
+lr_model = LinearRegression()
+x = df[['horsepower', 'curb-weight', 'engine-size', 'width']]
+y = df[['price']]
+lr_model.fit(x, y)
+
+#Hacer predicciones
+y_pred = lr_model.predict(x)
+
+#Calcular el R Squared
+from sklearn.metrics import r2_score
+r2 = r2_score(y, y_pred)
+print("R Squared:", r2)
